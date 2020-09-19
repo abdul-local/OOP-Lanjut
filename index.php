27c807cea9c7ddd7ac8a16b0c0c9ceaa2ce9713f
abdul-local <abdullah.hamzan@gmail.com>
2020-09-20 00:35:42 +0800

    class inheritence

--- index.php
+++ index.php
@@ -1,23 +1,82 @@
 <?php
-class lingkaran{
-    // dekalarasikan variabel
-    const PHI =3.14;
-    public $luas;
-    public $jari;
-    // buat method
-    public function setjari($jari2){
-        $this->jari = $jari2;
+// buat class baru dengan nama hero
+class hero {
+    // buat variabel
+    protected $healthpoint;
+    protected $magicpoint;
+    protected $name;
+    // buat method dengan nama shir
+    public function shiri(){
+        echo "$this->name  menyerang dengan sihir"."<br>";
+    }
+    // buat method dengan nama serang
+    public function nyerang(){
+        echo "$this->name menyerang dengan biasa "."<br>";
 
     }
-    // buat method hitung
-    public function hitung(){
-        return self::PHI * $this->jari * $this->jari;
+    // buat method dengan nama tahan
+    public function tahan(){
+        echo "$this->name Bertahan dari serangan musuh"."<br>";
     }
+    // buat method dengan nama gethealtpoint
+    public function gethealtpoint(){
+       return $this->healthpoint;
+    }
+    // buat method untuk getmagicpoint
+    public function getmegicpoint(){
+        return $this->magicpoint;
+    }
+}
+// buat sub class denga nama goku
+class songoku extends hero {
+    // buat method costruct
+    public function __construct()
+    {
+        $this->name = "Son Goku dragon ball";
+        $this->healthpoint=2000;
+        $this->magicpoint =1800;
+        
+    }
+}
+// buat sub class baru dengan nama narut
+class naruto extends hero {
+
+   // buat method construct
+   public function __construct()
+   {
+       $this->name = "naruto sipuden";
+       $this->healthpoint=1700;
+       $this->magicpoint=2000;
+   }
+    
 
 }
-$h= new lingkaran ();
-$h->setjari(10);
-echo $h->hitung();
+// buat class baru dengan nama avatar
+class avatar extends hero {
+    // buat method construct
+    public function __construct()
+    {
+        $this->name ="avatar ang ";
+        $this->healthpoint=1500;
+        $this->magicpoint=1700;
+    }
+}
+// buat sebuah instansiasi object
+$dragonball = new songoku();
+$dragonball->shiri();
+$dragonball->nyerang();
+$dragonball->tahan();
+echo "Hp: ".$dragonball->gethealtpoint()."<br>";
+echo "Mh: ".$dragonball->getmegicpoint()."<br>";
+
+// buat sebuah instansiasi object
+$naruto1 = new naruto();
+$naruto1->shiri();
+$naruto1->nyerang();
+$naruto1->tahan();
+echo "Hp: ".$naruto1->gethealtpoint()."<br>";
+echo "Mh: ".$naruto1->getmegicpoint()."<br>";
+
 
 
 
--- readme.md
+++ readme.md
@@ -10,5 +10,11 @@
 1. kita dapat mendeklarasikanya dengan const
 2. untuk merujuk ke konsta kita dapat menggunakan self
 
+# inheretence itu seperti pewarisan kelas ini 
+1. digunakan untuk penulisan code yang sama tidak perlu di tuli lagi
+
+# ada Namanya lagi N-Inheretence itu pawarisan subclass lagi di pecah jadi sublass lagi
+1. conthoh kita buat class utama dengan nama kelas1 , subclassnya dengan nama kelas2 , nah di class dua itu lagi kita buat subclass dengan
+nama misal kelas3 nah dari kelas2 dan kelas3 itu yang dinamakan dengan N-Inheretence
 
 
