5da4c23403e8dcd94b5d9ebe3a8a0fe0fc04c95e
abdul-local <abdullah.hamzan@gmail.com>
2020-09-19 22:20:09 +0800

    Class Constant

--- index.php
+++ index.php
@@ -1,88 +1,24 @@
 <?php
-class Car {
-
-    private $wheel;
-    private  $machine;
-    private  $seat;
-    // buat method dengan nama setWheel
-    public function setWheel($wd){
-        $this->wheel = $wd;
-    }
-    // buat method dengan nama setmachine
-    public function setmachine($machine){
-        $this->machine = $machine;
-    }
-    // buat method dengan nama setseat
-    public function setseat($seat){
-        $this->seat = $seat;
-    }
-    // buat method dengan ama getwheel
-    public function getwheel(){
-        return $this->wheel;
-    }
-    // buat method dengan nama getmachine
-    public function getmachine(){
-        return $this->machine;
+class lingkaran{
+    // dekalarasikan variabel
+    const PHI =3.14;
+    public $luas;
+    public $jari;
+    // buat method
+    public function setjari($jari2){
+        $this->jari = $jari2;
 
     }
-    // buat method dengan nama getseat
-    public function getseat(){
-        return $this->seat;
-    }
-    
-
-}
-// buat sub class dengan nama truc
-class Truct extends Car {
-    // buat method construct
-    public function __construct()
-    {
-        echo "Object Truct di buat .. "."<br>";
+    // buat method hitung
+    public function hitung(){
+        return self::PHI * $this->jari * $this->jari;
     }
 
 }
-// buat subclass dengan nama Excavator
-class Excavator extends Car {
-    public function __construct()
-    {
-        echo " Object Excavator dibuat .."."<br>";
-    }
+$h= new lingkaran ();
+$h->setjari(10);
+echo $h->hitung();
 
-}
-// buat subclass dengan nama Sport cart
-class SportCar extends Car {
-    public function __construct()
-    {
-        echo " Object SportCar dibuat .."."<br>";
-    }
-}
-
-// instansisasi 
-$t = new Truct();
- $t->setWheel(6);
- $t->setmachine("mesin Truct");
- $t->setseat(2);
-
- echo "Wheel :".$t->getwheel()."<br>";
- echo "machine: ".$t->getmachine()."<br>";
- echo "seat: ".$t->getseat()."<br>";
-
- $e= new Excavator();
-//  $e->setWheel(3);
-//  $e->setmachine("sport mobil");
-//  $e->setseat(3);
-
-$wheel = 20;
-$machine ="car asu";
-$seat=2;
-
-echo $wheel;
- 
- echo "Wheel: ".$e->getwheel()."<br>";
- echo "machine: ".$t->getmachine()."<br>";
- echo "seat: ".$t->getseat()."<br>";
-
- 
 
 
 ?>
--- readme.md
+++ readme.md
@@ -3,5 +3,12 @@
 
 # modifier protected 
 1. ini bisa di akses dengan subclass nya 
+# modfier private 
+1. ini hanya bisa di akses di kelas itu sendiri
+
+# untuk membuat constan pada php apasih printah yg di gunakan
+1. kita dapat mendeklarasikanya dengan const
+2. untuk merujuk ke konsta kita dapat menggunakan self
+
 
 
