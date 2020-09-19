2512557d5b8e5498167126dd5be256f2cc3bba09
abdul-local <abdullah.hamzan@gmail.com>
2020-09-19 21:38:58 +0800

    Modifier privat

--- index.php
+++ index.php
@@ -1,54 +1,88 @@
 <?php
-// buat class baru dengan nama persegi 
-class Persegi {
-    // buat modifier public
+class Car {
 
-    protected $panjang;
-    protected $lebar;
-    // buat method construct
-    public function __construct($p,$l)
-    {
-        $this->panjang = $p;
-        $this->lebar = $l;
+    private $wheel;
+    private  $machine;
+    private  $seat;
+    // buat method dengan nama setWheel
+    public function setWheel($wd){
+        $this->wheel = $wd;
     }
-    // buat method dengan nama luas
-    public function luas(){
-
-        return $this->panjang * $this->lebar ;
+    // buat method dengan nama setmachine
+    public function setmachine($machine){
+        $this->machine = $machine;
+    }
+    // buat method dengan nama setseat
+    public function setseat($seat){
+        $this->seat = $seat;
+    }
+    // buat method dengan ama getwheel
+    public function getwheel(){
+        return $this->wheel;
+    }
+    // buat method dengan nama getmachine
+    public function getmachine(){
+        return $this->machine;
 
     }
-    // buat methd dengan nama keliling
-    public function kelilig(){
-        return 2 *($this-> panjang + $this->lebar );
+    // buat method dengan nama getseat
+    public function getseat(){
+        return $this->seat;
     }
     
+
 }
-// buat class baru dengan nama balok dari kelas utamanya
-class Balok extends Persegi {
-    public $tinggi ;
-    public function __construct($p,$l,$t)
+// buat sub class dengan nama truc
+class Truct extends Car {
+    // buat method construct
+    public function __construct()
     {
-        $this->panjang = $p;
-        $this->lebar = $l;
-        $this->tinggi = $t;
+        echo "Object Truct di buat .. "."<br>";
     }
-    // buat method dengan nama luas
-    public function volume(){
-        return $this->panjang * $this->lebar * $this->tinggi ;
+
+}
+// buat subclass dengan nama Excavator
+class Excavator extends Car {
+    public function __construct()
+    {
+        echo " Object Excavator dibuat .."."<br>";
     }
-    // buat method baru dengan nama keliling
-    public function luas(){
-      return (4 * ($this->panjang * $this->lebar ) ) + (2 * ( $this->lebar * $this->lebar ));
+
+}
+// buat subclass dengan nama Sport cart
+class SportCar extends Car {
+    public function __construct()
+    {
+        echo " Object SportCar dibuat .."."<br>";
     }
 }
-$hitungan = new Balok(10,5,2);
-echo $hitungan->volume()."<br>";
-echo $hitungan->luas()."<br>";
 
-// $hitungan->panjang = 20;
-// $hitungan->lebar = 10;
-$hitungan->tinggi = 5;
+// instansisasi 
+$t = new Truct();
+ $t->setWheel(6);
+ $t->setmachine("mesin Truct");
+ $t->setseat(2);
 
-echo $hitungan->luas()."<br>";
-echo $hitungan->volume()."<br>";
+ echo "Wheel :".$t->getwheel()."<br>";
+ echo "machine: ".$t->getmachine()."<br>";
+ echo "seat: ".$t->getseat()."<br>";
+
+ $e= new Excavator();
+//  $e->setWheel(3);
+//  $e->setmachine("sport mobil");
+//  $e->setseat(3);
+
+$wheel = 20;
+$machine ="car asu";
+$seat=2;
+
+echo $wheel;
+ 
+ echo "Wheel: ".$e->getwheel()."<br>";
+ echo "machine: ".$t->getmachine()."<br>";
+ echo "seat: ".$t->getseat()."<br>";
+
+ 
+
+
 ?>
