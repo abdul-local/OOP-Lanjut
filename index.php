2e4d5ba2b4684236290351f3fccc37c632d0d1c8
abdul-local <abdullah.hamzan@gmail.com>
2020-09-19 17:25:39 +0800

    Memahami Constructor dan Destructor

--- index.php
+++ index.php
@@ -1,25 +1,52 @@
 <?php
-// buat class dgn nama mobil
-class mobil{
-    // buat sebuah varivel dengan nama statusbensin
-    private $statusbensin= false;
-    // buat sebuah method dengan naman isi bensin
-    public function isibensin(){
-        $this->statusbensin = true;
-
+// buat class dengan nama DB
+class Db {
+    //buat variable
+    private $statusconnection = "close";
+    // buat method construct
+    public function __construct()
+    {
+        $this->statusconnection = "Connecting";
     }
-    // buat method dengan nama nayalakan mobil
-    public function nyala(){
-        if($this->isibensin()){
-            return "Mobil sudah bisa jalan";
-        }else {
-            return "Mobil tidak ada bensin";
-        }
+   // buat method dengan nama connect
+    public function connect(){
+        $this->statusconnection = "Open";
+    }
+   // buat method denga nama getstatus Connection
+    public function getstatusconnection(){
+        return $this->statusconnection;
+    }
+    // buat method dengan __destruct 
+    public function __destruct()
+    {
+        $this->statusconnection = "close";
+        echo "status : ".$this->statusconnection;
+    }
+
+}
+// buat instansiasi  object dengan ama db
+$db = new Db();
+echo $db->getstatusconnection()."<br>";
+echo $db->connect()."<br>";
+echo $db->getstatusconnection()."<br>";
+
+//buat class baru dengan nama controller
+class controller {
+    // buat method consrtuct
+    public function __construct()
+    {
+        echo "Ini dalam construct broo"."<br>";
+    }
+
+}
+// buat sub class dengan nama homecontroller 
+class homecontrollr extends controller {
+    // buat method baru dengan nama consdtruct
+    public function __construct()
+    {
+        parent::__construct();
+        echo "Ini dalam Construct broo"."<br>";
     }
 }
-
-// buat instansi object dengan nama mobil saya
-$mobilsaya = new mobil();
-// cetak isi bensin
-echo $mobilsaya->isibensin();
-echo $mobilsaya->nyala();
+// instansi dengan nama abdul
+$abdul = new homecontrollr();
